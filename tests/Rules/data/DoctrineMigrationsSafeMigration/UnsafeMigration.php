51d60a8738dda124eb3e9ec8f7f72ef6926f50db
Vincent LE BIANNIC <vincent.le-biannic@niji.fr>
2025-10-01 16:32:19 +0200

    DoctrineMigrationsSafeMigration: use standard phpstan-ignore comment instead of a custom PHPDoc tag

--- tests/Rules/data/DoctrineMigrationsSafeMigration/UnsafeMigration.php
+++ tests/Rules/data/DoctrineMigrationsSafeMigration/UnsafeMigration.php
@@ -11,26 +11,26 @@ class UnsafeMigration extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        // Unsafe operations, not tagged as safe
+        // Unsafe operations, not ignored
         $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE user CHANGE name email VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE user DROP COLUMN name');
         $this->addSql('DROP DATABASE test');
         $this->addSql('TRUNCATE TABLE user');
 
-        // Unsafe operations, tagged as safe
-        /** @safe-migration */
+        // Unsafe operations, ignored
+        /* @phpstan-ignore doctrineMigrations.unsafeMigration */
         $this->addSql('DROP TABLE user');
-        /** @safe-migration */
+        /* @phpstan-ignore doctrineMigrations.unsafeMigration */
         $this->addSql('ALTER TABLE user CHANGE name email VARCHAR(255) NOT NULL');
-        /** @safe-migration */
+        /* @phpstan-ignore doctrineMigrations.unsafeMigration */
         $this->addSql('ALTER TABLE user DROP COLUMN name');
 
-        // Some unsafe operations again to make sure that the tag does not apply to them
+        // Some unsafe operations again to make sure that the ignore tags do not apply to them
         $this->addSql('DROP TABLE user');
         $this->addSql('TRUNCATE TABLE user');
 
-        // Safe operations (do not impact backward compatibility and do not need to be tagged as safe)
+        // Safe operations (do not impact backward compatibility and do not need to be ignored)
         $this->addSql('ALTER TABLE user ADD COLUMN name VARCHAR(255) NOT NULL');
         $this->addSql('SELECT * FROM user');
         $this->addSql("INSERT INTO user (name) VALUES ('John')");
